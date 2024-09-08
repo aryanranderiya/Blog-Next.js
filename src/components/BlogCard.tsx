1cc128752a258c4e5d8510e309fde8f4a35a4a7a
Aryan Randeriya <aryanranderiya1478@gmail.com>
2024-09-08 17:54:04 +0530

    update page.tsx and BlogCard.tsx

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -38,8 +38,7 @@ export default function BlogCard({ post }: { post: Post }) {
     <Link
       className="flex sm:w-[300px] w-full min-h-[390px] max-h-[390px] bg-foreground-100 rounded-xl p-[10px] relative flex-col cursor-pointer hover:-tran  ate-y-2 hover:bg-foreground-200 transition-all"
       onMouseOut={handleMouseOut}
-      // href={post?.postID?.toString() || "/"}
-      href={"/"}
+      href={post?.postID?.toString() || "/allposts"}
       onMouseOver={handleMouseOver}
     >
       <div className="min-h-[130px] max-h-[130px] overflow-hidden  rounded-xl bg-foreground-300 ">
